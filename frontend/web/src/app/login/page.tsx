d02558a02185ce9196b48f0d0fcbd00040592db0
dylanO9 <dylan.olivares11@gmail.com>
2025-05-17 17:13:51 -0700

    Created signup and login page
    Signup and login page created. They both feature error handling, so if failed a message will appear at the bottom of the form. There is an included loading screen

---
+++ frontend/web/src/app/login/page.tsx
@@ -0,0 +1,108 @@
+'use client';
+import Link from "next/link";
+import { useState } from "react"
+import { useRouter } from 'next/navigation';
+export default function Login () {
+    const router = useRouter();
+    const [form, setForm] = useState({ username: '', password: ''});
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+      };
+    
+    const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError(false);
+    setLoading(true);
+    // Optionally send data to an API route
+    try {
+        const response = await fetch('http://localhost:5001/api/users/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(form),
+        });
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        } 
+        const data = await response.json();
+
+        // Save the JWT token to local storage
+        localStorage.setItem('token', data.token);
+
+        if (data.error) {
+            setError(true);
+            setLoading(false);
+            return;
+        }
+        // Handle success state here
+        console.log('Login successful:', data);
+
+        // Add a one second delay
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        setLoading(false);
+
+        // Optionally redirect or update UI
+        router.push('/');
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+        setError(true);
+        setLoading(false);
+        return;
+    }
+    };
+    
+    if (loading) {
+        return (
+            <div className="flex items-center justify-center min-h-screen">
+                <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-purple-400 border-solid"></div>
+            </div>
+        );
+    }
+
+    return (
+        <main className="flex flex-col min-h-screen items-center justify-center">
+            <div className="max-w-lg w-md w-full">
+                <h1 className="mb-8 text-lg text-center text-gray-500">Welcome back! Log in to your account</h1>
+
+                <form onSubmit={handleSubmit} className="bg-white border-1 border-gray-300 px-8 py-4 rounded-md shadow-md">
+                    <h2 className="font-semibold text-xl">Log In</h2>
+                    <h3 className="mb-8 text-gray-500 text-sm">Enter your credentials to access your account</h3>
+
+                    <label className="block text-sm font-semibold mb-1" htmlFor="username">Username</label>
+                    <input
+                        className="block rounded-md border-gray-300 bg-off-white border-1 w-full mb-4 p-1"
+                        id="username"
+                        name="username"
+                        value={form.username}
+                        onChange={handleChange}
+                        required
+                        type="text"
+                        placeholder="Test_User"
+                    />
+
+                    <label className="block text-sm font-semibold mb-1" htmlFor="password">Password</label>
+                    <input
+                        className="block rounded-md border-gray-300 bg-off-white border-1 w-full mb-4 p-1"
+                        id="password"
+                        name="password"
+                        value={form.password}
+                        onChange={handleChange}
+                        required
+                        type="text"
+                        placeholder="************"
+                    />
+                    <button type="submit" className="w-full p-2 rounded-md bg-purple-400 mb-2" disabled={loading}>Log In</button>
+                    <p className="text-sm text-muted-foreground">
+                        Don't have an account?{" "}
+                        <Link href="/signup" className="text-fitness-primary hover:underline font-medium text-purple-400">
+                            Sign up
+                        </Link>
+                    </p>
+                    {error && <p className="text-red-500 text-sm mt-2">Log in failed. Please try again.</p>}
+                </form>
+            </div>
+        </main>
+    )
+}
---
+++ frontend/web/src/app/signup/page.tsx
@@ -0,0 +1,122 @@
+'use client';
+import Link from "next/link";
+import { useState } from "react"
+import { useRouter } from 'next/navigation';
+export default function Signup () {
+    const router = useRouter();
+    const [form, setForm] = useState({ email: '', username: '', password: ''});
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+      };
+    
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setError(false);
+        setLoading(true);
+        try {
+            const response = await fetch('http://localhost:5001/api/users/signup', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(form),
+            });
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            } 
+            const data = await response.json();
+    
+            // Save the JWT token to local storage
+            localStorage.setItem('token', data.token);
+    
+            if (data.error) {
+                setError(true);
+                setLoading(false);
+                return;
+            }
+            // Handle success state here
+            console.log('Signup successful:', data);
+
+            // Add a one second delay
+            await new Promise(resolve => setTimeout(resolve, 1000));
+
+            setLoading(false);
+
+            // Optionally redirect or update UI
+            router.push('/');
+        }
+        catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+            setError(true);
+            setLoading(false);
+            return;
+        }
+        setLoading(false);
+    };
+    
+    if (loading) {
+        return (
+            <div className="flex items-center justify-center min-h-screen">
+                <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-purple-400 border-solid"></div>
+            </div>
+        );
+    }
+
+    return (
+        <main className="flex flex-col min-h-screen items-center justify-center">
+            <div className="max-w-lg w-md w-full">
+                <h1 className="mb-8 text-lg text-center text-gray-500">Join The Fitness Community Today</h1>
+
+                <form onSubmit={handleSubmit} className="bg-white border-1 border-gray-300 px-8 py-4 rounded-md shadow-md">
+                    <h2 className="font-semibold text-xl">Create an Account</h2>
+                    <h3 className="mb-8 text-gray-500 text-sm">Enter your information to get started</h3>
+                    
+                    <label className="block text-sm font-semibold mb-1" htmlFor="username">Email</label>
+                    <input
+                        className="block rounded-md border-gray-300 bg-off-white border-1 w-full mb-4 p-1"
+                        id="email"
+                        name="email"
+                        value={form.email}
+                        onChange={handleChange}
+                        required
+                        type="email"
+                        placeholder="TestUser@gmail.com"
+                    />
+
+                    <label className="block text-sm font-semibold mb-1" htmlFor="username">Username</label>
+                    <input
+                        className="block rounded-md border-gray-300 bg-off-white border-1 w-full mb-4 p-1"
+                        id="username"
+                        name="username"
+                        value={form.username}
+                        onChange={handleChange}
+                        required
+                        type="text"
+                        placeholder="Test_User"
+                    />
+
+                    <label className="block text-sm font-semibold mb-1" htmlFor="password">Password</label>
+                    <input
+                        className="block rounded-md border-gray-300 bg-off-white border-1 w-full mb-4 p-1"
+                        id="password"
+                        name="password"
+                        value={form.password}
+                        onChange={handleChange}
+                        required
+                        type="text"
+                        placeholder="************"
+                    />
+                    <button type="submit" className="w-full p-2 rounded-md bg-purple-400 mb-2" disabled={loading}>Sign up</button>
+                    <p className="text-sm text-muted-foreground">
+                        Already have an account?{" "}
+                        <Link href="/login" className="text-fitness-primary hover:underline font-medium text-purple-400">
+                            Log In
+                        </Link>
+                    </p>
+                    {error && <p className="text-red-500 text-sm mt-2">Sign up failed. Please try again.</p>}
+                </form>
+            </div>
+        </main>
+    )
+}
