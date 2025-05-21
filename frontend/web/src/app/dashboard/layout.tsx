9c638192054c5cca915273a131fc6dcb6511044b
DylanO9 <dylan.olivares11@gmail.com>
2025-05-21 15:29:23 -0700

    Fix unused vars

--- frontend/web/src/app/dashboard/layout.tsx
+++ frontend/web/src/app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import "../globals.css";
 import Link from "next/link";
-import { FaHome, FaRss, FaDumbbell, FaCalendarAlt, FaChalkboardTeacher, FaEnvelope, FaSignOutAlt } from "react-icons/fa";
+import { FaHome, FaRss, FaDumbbell, FaCalendarAlt, FaEnvelope, FaSignOutAlt } from "react-icons/fa";
 import { AuthProvider } from "../context/AuthContext";
 
 export default async function DashboardLayout({
