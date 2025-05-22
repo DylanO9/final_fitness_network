06bf9ea1d03b740c8202927a59e62bbe8e2eba49
DylanO9 <dylan.olivares11@gmail.com>
2025-05-21 17:26:57 -0700

    Update index.js

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -4,14 +4,7 @@ require('dotenv').config();
 let pool;
 try {
   pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: {
-      rejectUnauthorized: false,
-    },
+    connectionString: process.env.DB.DATABASE.URL
   });
   console.log('Database connected successfully');
 } catch (error) {
